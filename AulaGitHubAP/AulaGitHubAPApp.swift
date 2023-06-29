6b8f9995f2554c22453ff9f284171cc5d991539d
Amanda Conde <flab.amanda@gmail.com>
2023-06-29 16:18:09 -0300

    Initial Commit

---
+++ AulaGitHubAP/AulaGitHubAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaGitHubAPApp.swift
+//  AulaGitHubAP
+//
+//  Created by Amanda Conde on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AulaGitHubAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AulaGitHubAP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AulaGitHubAP
+//
+//  Created by Amanda Conde on 29/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
